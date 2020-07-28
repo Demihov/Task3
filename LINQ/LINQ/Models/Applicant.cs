f497a13f0e825572d8b57815fa8636b5d3873b09
Demihov <32543463+Demihov@users.noreply.github.com>
2020-07-28 23:27:27 +0300

    First commit

---
+++ LINQ/LINQ/Models/Applicant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ.Models
+{
+    public class Applicant
+    {
+        public int SchoolNumber { get; }
+        public string LastName { get; }
+        public int YearOfEntry { get; }
+
+        public Applicant(int num, int year, string lastName)
+        {
+            SchoolNumber = num;
+            LastName = lastName;
+            YearOfEntry = year;
+        }
+    }
+}
